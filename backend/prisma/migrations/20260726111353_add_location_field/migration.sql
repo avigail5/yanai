ae51e233db91e663f67716ac61f74aefea6fb472
avigail5 <avigailfsl@gmail.com>
2026-07-29 13:48:35 +0300

    fix the migration to add the extention in public

--- backend/prisma/migrations/20260726111353_add_location_field/migration.sql
+++ backend/prisma/migrations/20260726111353_add_location_field/migration.sql
@@ -1,3 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;
 -- AlterTable
 ALTER TABLE "hafifa"."tasks" ADD COLUMN "location" public.geometry(Point, 4326);
